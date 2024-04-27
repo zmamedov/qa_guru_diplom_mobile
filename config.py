926200ac812572d33a01724ac171deed0aa7b554
Zaur Mamedov <zaurmd1996@gmail.com>
2024-04-27 20:28:29 +0300

    Add pydantic to config.py

--- config.py
+++ config.py
@@ -1,17 +1,28 @@
 import os
-
+import pydantic
 from appium.options.android import UiAutomator2Options
 from qa_guru_diplom_mobile.utils.path import abs_path_from_project
 
 remote_url = os.getenv('remote_url', 'http://127.0.0.1:4723')
 
 
+class Config(pydantic.BaseSettings):
+    remote_url: str = 'http://127.0.0.1:4723'
+    platformName: str = 'android'
+    deviceName: str = 'emulator-5554'
+    appWaitActivity: str = 'org.wikipedia.*'
+    app: str = abs_path_from_project('app-alpha-universal-release.apk')
+
+
+config = Config()
+
+
 def to_driver_options():
     options = UiAutomator2Options()
 
-    options.set_capability('platformName', os.getenv('platformName', 'android'))
-    options.set_capability('deviceName', os.getenv('deviceName', 'emulator-5554'))
-    options.set_capability('appWaitActivity', os.getenv('appWaitActivity', 'org.wikipedia.*'))
-    options.set_capability('app', os.getenv('app', abs_path_from_project('app-alpha-universal-release.apk')))
+    options.set_capability('platformName', config.platformName)
+    options.set_capability('deviceName', config.deviceName)
+    options.set_capability('appWaitActivity', config.appWaitActivity)
+    options.set_capability('app', config.app)
 
     return options
