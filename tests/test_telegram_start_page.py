8700dd102c04f24905453229fb7a6b8df1fce39b
Zaur Mamedov <zaurmd1996@gmail.com>
2024-05-01 16:32:49 +0300

    Small fix of tests

--- tests/test_telegram_start_page.py
+++ tests/test_telegram_start_page.py
@@ -19,8 +19,10 @@ def test_start_messaging():
         browser.element((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Start Messaging")')).click()
     with allure.step('Click the button "Continue".'):
         browser.element((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Continue")')).click()
-    with allure.step('Deny Telegram to make and manage phone calls.'):
-        browser.element((AppiumBy.ID, 'com.android.packageinstaller:id/permission_deny_button')).click()
+    with (allure.step('Deny Telegram to make and manage phone calls.')):
+        browser.element((AppiumBy.ANDROID_UIAUTOMATOR,
+                         'new UiSelector().resourceId("com.android.permissioncontroller:id/permission_deny_button")')
+                        ).click()
 
     with allure.step('Page for filling phone number is displaying.'):
         browser.element((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Your phone number")')).should(
@@ -38,9 +40,7 @@ def test_switch_theme():
         browser.element((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Telegram")')).should(
             have.text('Telegram'))
 
-    with allure.step('Change the theme on dark'):
-        browser.element((AppiumBy.CLASS_NAME, 'android.widget.ImageView')).click()
-    with allure.step('Return to the light theme'):
+    with allure.step('Change the theme on dark.'):
         browser.element((AppiumBy.CLASS_NAME, 'android.widget.ImageView')).click()
 
 
@@ -56,8 +56,10 @@ def test_type_phone_number():
     with allure.step('Click the button "Continue".'):
         browser.element((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Continue")')).click()
     with allure.step('Deny Telegram to make and manage phone calls.'):
-        browser.element((AppiumBy.ID, 'com.android.packageinstaller:id/permission_deny_button')).click()
-    with allure.step('Clickt on list of values "Country".'):
+        browser.element((AppiumBy.ANDROID_UIAUTOMATOR,
+                         'new UiSelector().resourceId("com.android.permissioncontroller:id/permission_deny_button")')
+                        ).click()
+    with allure.step('Click on list of values "Country".'):
         browser.element(
             (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.TextView").instance(2)')).click()
     with allure.step('Choose the country "Australia".'):
